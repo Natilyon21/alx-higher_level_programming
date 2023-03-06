4338ed8abdbb526fd8c55d9755ff0bd3ce9d1c0c
natilyon21 <thelasthockage@gmail.com>
2023-03-06 12:18:57 -0800

    l

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 number = 98
-<<<<<<< HEAD
-print("{number:d}, Battery street")
-=======
-print("{:d} Battery street".format(number))
->>>>>>> b8799f37f0591cf7a4b42bb0b33202a53b71813f
+print(f"{number:d} Battery street")
