53025a24f61261747149daae12c9d0afd1290c05
Natilyon21 <105010511+Natilyon21@users.noreply.github.com>
2023-03-08 11:21:31 -0800

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        n = chr(i)
+for i in reversed(range(97, 123)):
+    if (i % 2 == 0):
+        print('{:c}'.format(i), end='')
     else:
-        n = chr(i-32)
-    print("{}".format(n), end="")
+        print('{:c}'.format(i - 32), end='')
