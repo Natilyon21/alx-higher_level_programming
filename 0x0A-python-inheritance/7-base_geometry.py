559636b2467655972fa3dddeea59d7aad4393e3c
Natilyon21 <105010511+Natilyon21@users.noreply.github.com>
2023-04-11 13:51:51 -0700

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+Contain the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """a class with public instance methods"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
